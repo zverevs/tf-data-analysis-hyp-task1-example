185b62b51efe2fe7cf417b09cc7d40380a969c91
zverevs <129664415+zverevs@users.noreply.github.com>
2023-04-12 20:46:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+import scipy.stats as stats
+from statsmodels.stats.proportion import proportions_ztest
 
 chat_id = 235789175 # Ваш chat ID, не меняйте название переменной
 
